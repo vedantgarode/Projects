f7d2d0512d6c7a1e677207d653a4ed7b8a3d58b5
Spartex <vedantgarode11@gmail.com>
2021-10-13 17:28:31 +0530

    Done

--- Company_Management/main.py
+++ Company_Management/main.py
@@ -1,1 +1,8 @@
-import PyQt5
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+app = QApplication(sys.argv)
+w = QWidget()
+w.resize(300,300)
+w.setWindowTitle("Ha vai")
+w.show()
+sys.exit(app.exec_())
