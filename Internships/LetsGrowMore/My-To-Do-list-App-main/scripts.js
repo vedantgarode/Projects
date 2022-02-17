02703673bbe94fc7e0eb21fe69d80d16987e9f2f
Spartex <vedantgarode11@gmail.com>
2022-02-17 21:31:00 +0530

    todo app

---
+++ Internships/LetsGrowMore/My-To-Do-list-App-main/scripts.js
@@ -0,0 +1,55 @@
+let index = 1;
+function adder() {
+  console.log("Added Succesfully");
+  task = document.getElementById('input-text').value;
+  if (localStorage.getItem('itemsjson') == null) {
+    taskarray = [];
+    taskarray.push([task]);
+    localStorage.setItem('itemsjson', JSON.stringify(taskarray))
+    
+  }
+  else {
+    taskarrayStr = localStorage.getItem('itemsjson');
+    taskarray = JSON.parse(taskarrayStr)
+    taskarray.push([task])
+    localStorage.setItem('itemsjson', JSON.stringify(taskarray))
+  }
+  update();
+  document.getElementById('input-text').value=""
+}
+function remove(itemidx) {
+  taskarrayStr = localStorage.getItem('itemsjson');
+  taskarray = JSON.parse(taskarrayStr)
+  taskarray.splice(itemidx,1);
+  localStorage.setItem('itemsjson', JSON.stringify(taskarray))
+  console.log("removed successfully" + itemidx)
+  update();
+}
+function update() {
+  taskarrayStr = localStorage.getItem('itemsjson');
+  taskarray = JSON.parse(taskarrayStr)
+  let table = document.getElementById("table-body");
+  let str = "";
+
+  taskarray.forEach((element, index) => {
+    str +=
+      `<tr>
+    <td width="4%">${index + 1}</td>
+    <td width="90%">${element[0]}</td>
+    <td class="remove-button" onclick="remove(${index })">Remove</td>
+    </tr>`
+  });
+  table.innerHTML = str;
+  console.log("Cleared")
+}
+function clear() {
+  localStorage.clear();
+  let table = document.getElementById("table-body");
+  table.innerHTML="";
+  document.getElementById('input-text').value=""
+}
+add = document.getElementById("adder");
+add.addEventListener("click", adder);
+
+clr = document.getElementById("clear");
+clr.addEventListener("click", clear)
